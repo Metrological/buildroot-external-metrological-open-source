6e09fb8d71baf286afcdc3fcf67bc1c77b3eeeb2
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2022-08-24 20:14:39 +0200

    bump kms-cube

--- package/kms-cube/kms-cube.mk
+++ package/kms-cube/kms-cube.mk
@@ -3,19 +3,10 @@
 # kms-cube
 #
 ################################################################################
-
-KMS_CUBE_VERSION = 4660a7dca6512b6e658759d00cff7d4ad2a2059d
-KMS_CUBE_SITE = https://gitlab.freedesktop.org/mesa/kmscube/-/archive/$(KMSCUBE_VERSION)
+KMS_CUBE_VERSION = 9f63f359fab1b5d8e862508e4e51c9dfe339ccb0
+KMS_CUBE_SITE = https://gitlab.freedesktop.org/mesa/kmscube/-/archive/$(KMS_CUBE_VERSION)
 KMS_CUBE_LICENSE = MIT
 KMS_CUBE_LICENSE_FILES = COPYING
-KMS_CUBE_DEPENDENCIES = host-pkgconf libdrm host-patchelf
-
-#hack to remove a local path from the NEEDED list
-define KMS_CUBE_FIX_NEEDED
-     $(HOST_DIR)/usr/bin/patchelf --replace-needed $(STAGING_DIR)/usr/lib/libgbm.so libgbm.so $(@D)/build/kmscube
-     $(HOST_DIR)/usr/bin/patchelf --replace-needed $(STAGING_DIR)/usr/lib/libgbm.so libgbm.so $(@D)/build/texturator
-endef
-
-KMS_CUBE_POST_BUILD_HOOKS += KMS_CUBE_FIX_NEEDED
+KMS_CUBE_DEPENDENCIES = host-pkgconf host-patchelf libdrm libegl libgbm libgles 
 
 $(eval $(meson-package))
