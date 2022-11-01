37b08ed2e6410cff96393d502d2601f22d1671bb
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2022-11-01 11:42:52 +0100

    wpeframework-tools: makefile cleanup

--- package/wpeframework-opensource/wpeframework-tools/wpeframework-tools.mk
+++ package/wpeframework-opensource/wpeframework-tools/wpeframework-tools.mk
@@ -4,10 +4,8 @@
 #
 ################################################################################
 WPEFRAMEWORK_TOOLS_VERSION = dfc519efec22bdae70d86fd5e364e764f0021b53
-HOST_WPEFRAMEWORK_TOOLS_SITE = $(call github,rdkcentral,Thunder,$(WPEFRAMEWORK_TOOLS_VERSION))
-HOST_WPEFRAMEWORK_TOOLS_INSTALL_STAGING = YES
-HOST_WPEFRAMEWORK_TOOLS_INSTALL_TARGET = NO
-HOST_WPEFRAMEWORK_TOOLS_DEPENDENCIES = host-cmake host-python3 host-python3-jsonref
+WPEFRAMEWORK_TOOLS_SITE = $(call github,rdkcentral,Thunder,$(WPEFRAMEWORK_TOOLS_VERSION))
+HOST_WPEFRAMEWORK_TOOLS_DEPENDENCIES = host-python3 host-python3-jsonref
 HOST_WPEFRAMEWORK_TOOLS_SUBDIR = Tools
 
 ifeq ($(BR2_CMAKE_HOST_DEPENDENCY),)
