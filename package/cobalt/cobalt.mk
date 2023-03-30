783113d017aae4afcff35ed8e6078213411aedd3
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2023-03-30 19:00:59 +0200

    cobalt: move to cobalt 23

--- package/cobalt/cobalt.mk
+++ package/cobalt/cobalt.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-COBALT_VERSION = 3ba98a48be9cc4d6a8cc04d74e85ca9f830e13ee
+COBALT_VERSION = a772cccf1da68623f3312bb8466860e2d1964f24
 COBALT_SITE_METHOD = git
 COBALT_SITE = git@github.com:Metrological/cobalt
 COBALT_INSTALL_STAGING = YES
-COBALT_DEPENDENCIES = gst1-plugins-good gst1-plugins-bad host-bison host-ninja wpeframework-clientlibraries
+COBALT_DEPENDENCIES = gst1-plugins-good gst1-plugins-bad host-bison host-ninja wpeframework-clientlibraries host-python3 host-python-six host-gn
 
 export COBALT_STAGING_DIR=$(STAGING_DIR)
 export COBALT_TOOLCHAIN_PREFIX=$(TARGET_CROSS)
@@ -16,17 +16,19 @@ export COBALT_INSTALL_DIR=$(TARGET_DIR)
 
 export PATH := $(HOST_DIR)/bin:$(HOST_DIR)/usr/bin:$(HOST_DIR)/usr/sbin:$(PATH)
 
-ifeq ($(BR2_PACKAGE_HAS_NEXUS),y)
-# TODO: we might also have mips here at some point.
-COBALT_PLATFORM = wpe-brcm-arm
-COBALT_DEPENDENCIES += gst1-bcm
-else
 ifeq ($(BR2_ARCH_IS_64),y)
-COBALT_PLATFORM = wpe-rpi64
+COBALT_PLATFORM = wpe-arm64
+COBALT_CPU_ARCH = arm64
 else
-COBALT_PLATFORM = wpe-rpi
+COBALT_PLATFORM = wpe-arm
+COBALT_CPU_ARCH = arm
 endif
 
+ifeq ($(BR2_PACKAGE_HAS_NEXUS),y)
+COBALT_DEPENDENCIES += gst1-bcm
+COBALT_VIDEO_OVERLAY=false
+else
+COBALT_VIDEO_OVERLAY=true
 endif
 
 ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_CLIENTLIBRARY_CDM),y)
@@ -62,7 +64,7 @@ export COBALT_EXECUTABLE_TYPE = executable
 endif
 
 ifeq ($(BR2_PACKAGE_COBALT_USE_WPEFRAMEWORK_DATA_PATH), y)
-export COBALT_DATA_PATH = "$(subst ",,$(BR2_PACKAGE_WPEFRAMEWORK_DATA_PATH)")/Cobalt"
+export COBALT_DATA_PATH = $(subst ",,$(BR2_PACKAGE_WPEFRAMEWORK_DATA_PATH)")/Cobalt
 COBALT_DATA_INSTALL_PATH = $(TARGET_DIR)/$(COBALT_DATA_PATH)
 COBALT_DATA_SRC_PATH = content/data/*
 else
@@ -71,14 +73,23 @@ COBALT_DATA_SRC_PATH = content
 endif
 
 define COBALT_BUILD_CMDS
-    $(@D)/src/cobalt/build/gyp_cobalt -C $(COBALT_BUILD_TYPE) $(COBALT_PLATFORM)
-    $(HOST_DIR)/usr/bin/ninja -C $(@D)/src/out/$(COBALT_PLATFORM)_$(COBALT_BUILD_TYPE) cobalt_deploy
+    export PYTHONPATH="$(PYTHONPATH):$(@D)"; \
+    cd $(@D) && $(HOST_DIR)/usr/bin/gn gen out/wpe --script-executable=python3 --args='target_platform="$(COBALT_PLATFORM)" build_type="$(COBALT_BUILD_TYPE)" target_cpu="$(COBALT_CPU_ARCH)" is_clang=false sb_install_content_subdir="content/data" is_video_overlay=$(COBALT_VIDEO_OVERLAY)'
+    $(HOST_DIR)/usr/bin/ninja -C $(@D)/out/wpe cobalt_install
+endef
+
+define COBALT_INSTALL_STAGING_CMDS
+    cp -a $(@D)/out/wpe/install/lib/libcobalt.so $(STAGING_DIR)/usr/lib/libcobalt.so
+    mkdir -p $(STAGING_DIR)/usr/include/starboard
+    cp -a $(@D)/starboard/*.h $(STAGING_DIR)/usr/include/starboard/
+    mkdir -p $(STAGING_DIR)/usr/include/third_party/starboard/wpe/shared
+    cp -a $(@D)/third_party/starboard/wpe/shared/*.h $(STAGING_DIR)/usr/include/third_party/starboard/wpe/shared/
 endef
 
 define COBALT_INSTALL_TARGET_CMDS
+    cp -a $(@D)/out/wpe/install/lib/libcobalt.so $(TARGET_DIR)/usr/lib/libcobalt.so
     mkdir -p $(COBALT_DATA_INSTALL_PATH)
-    cp -a $(@D)/src/out/$(COBALT_PLATFORM)_$(COBALT_BUILD_TYPE)/$(COBALT_DATA_SRC_PATH) $(COBALT_DATA_INSTALL_PATH)
+    cp -a $(@D)/out/wpe/install/$(COBALT_DATA_SRC_PATH) $(COBALT_DATA_INSTALL_PATH)
 endef
-
 
 $(eval $(generic-package))
