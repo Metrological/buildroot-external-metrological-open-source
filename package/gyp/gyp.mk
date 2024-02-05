bba1dbf04d4cabf26d23c3f4002f17d1398b6e26
Bram Oosterhuis <dev@bybram.com>
2024-02-05 12:15:12 +0100

    gyp: bump version

--- package/gyp/gyp.mk
+++ package/gyp/gyp.mk
@@ -4,25 +4,25 @@
 #
 ################################################################################
 
-GYP_VERSION = e87d37d6bce611abed35e854d5ae1a401e9ce04c
+GYP_VERSION = 2d207fd1699c88dcf6dfdd819d940c0dd227ffe7
 GYP_SITE_METHOD = git
 GYP_SITE = https://chromium.googlesource.com/external/gyp
 GYP_INSTALL_STAGING = NO
 GYP_SETUP_TYPE = setuptools
 GYP_DEPENDENCIES = host-gyp 
-HOST_GYP_DEPENDENCIES = host-python3 host-python-setuptools 
+HOST_GYP_DEPENDENCIES = host-python3 host-python-setuptools
 
 define HOST_GYP_CONFIGURE_CMDS
 	(cd $(@D); rm -rf build)
 endef
 
 define HOST_GYP_BUILD_CMDS
-	$(HOST_MAKE_ENV) PYTHON=$(HOST_DIR)/usr/bin/python2;
+	$(HOST_MAKE_ENV) PYTHON=$(HOST_DIR)/usr/bin/python3;
 	cd $(@D);$(PYTHON) ./setup.py build;
 endef
 
 define HOST_GYP_INSTALL_CMDS
-        $(HOST_MAKE_ENV) PYTHON=$(HOST_DIR)/usr/bin/python2;
+        $(HOST_MAKE_ENV) PYTHON=$(HOST_DIR)/usr/bin/python3;
         cd $(@D);$(PYTHON) ./setup.py install;
 endef
 
