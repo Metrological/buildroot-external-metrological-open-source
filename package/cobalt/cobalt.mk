f2a88d42f4952c4a9a528ad053de0fbe6225f77d
Bram Oosterhuis <dev@bybram.com>
2024-02-24 11:23:05 +0100

    cobalt: bump version

--- package/cobalt/cobalt.mk
+++ package/cobalt/cobalt.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-COBALT_VERSION = cc9bac9d547a0a9dda8aa8726e0e0ddd4b9b3ef1
+COBALT_VERSION = c0cf16cc7a2207ca51bbb3c6724eea530003c69a
 COBALT_SITE = $(call github,Metrological,cobalt,$(COBALT_VERSION))
 COBALT_INSTALL_STAGING = YES
 COBALT_DEPENDENCIES = gst1-plugins-good gst1-plugins-bad host-bison host-ninja wpeframework-clientlibraries host-python3 host-gn
