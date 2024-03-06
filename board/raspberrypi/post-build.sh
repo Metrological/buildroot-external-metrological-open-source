af31e70b862d6c34fff9a08006b7a0657cbe97ec
Bram Oosterhuis <dev@bybram.com>
2024-03-06 15:23:44 +0100

    post-build: fix tty1 option

--- board/raspberrypi/post-build.sh
+++ board/raspberrypi/post-build.sh
@@ -12,7 +12,7 @@ do
                     echo "Enable HDMI console"
                     grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
                     sed -i '/GENERIC_SERIAL/a\
-                tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
                 fi
                 ;;
 
