82e8eb6efae5a676a580d3cec03f49d99089ca1e
Bram Oosterhuis <dev@bybram.com>
2024-06-20 13:40:09 +0200

    external: Add variables to check versions and changes

--- external.mk
+++ external.mk
@@ -1,3 +1,8 @@
+EXTERNAL_ML_OSS_REFERENCE=$(shell pushd $(BR2_EXTERNAL_ML_OSS_PATH) &> /dev/null && $(GIT) rev-parse --short HEAD; popd &> /dev/null)
+EXTERNAL_ML_OSS_DIRTY=$(shell pushd $(BR2_EXTERNAL_ML_OSS_PATH) &> /dev/null && $(GIT) diff --shortstat 2>/dev/null | wc -l ; popd &> /dev/null)
+
+BUILDROOT_REFERENCE=$(shell $(GIT) rev-parse --short HEAD)
+BUILDROOT_DIRTY=$(shell $(GIT) diff --shortstat 2>/dev/null | wc -l)
+
 include $(sort $(wildcard $(BR2_EXTERNAL_ML_OSS_PATH)/package/*/*.mk))
 include $(sort $(wildcard $(BR2_EXTERNAL_ML_OSS_PATH)/adaptation/*/*.mk))
-
