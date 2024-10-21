499325789ebe19742d7163af745387e8752f4480
Bram Oosterhuis <dev@bybram.com>
2024-10-21 11:16:08 +0200

    thunder-cdmi: bump to 5.1.0

--- package/thunder-opensource/thunder-cdmi/thunder-cdmi-clearkey/thunder-cdmi-clearkey.mk
+++ package/thunder-opensource/thunder-cdmi/thunder-cdmi-clearkey/thunder-cdmi-clearkey.mk
@@ -3,7 +3,7 @@
 # thunder-cdmi-clearkey
 #
 ################################################################################
-THUNDER_CDMI_CLEARKEY_VERSION = R5.0.0
+THUNDER_CDMI_CLEARKEY_VERSION = R5.1.0
 THUNDER_CDMI_CLEARKEY_SITE = $(call github,rdkcentral,OCDM-Clearkey,$(THUNDER_CDMI_CLEARKEY_VERSION))
 THUNDER_CDMI_CLEARKEY_INSTALL_STAGING = YES
 THUNDER_CDMI_CLEARKEY_DEPENDENCIES = thunder-clientlibraries libopenssl
