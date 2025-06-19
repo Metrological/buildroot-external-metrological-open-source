49ff0613bbd1168dab72d6ee283138aa8368d44a
Bram Oosterhuis <dev@bybram.com>
2025-06-19 12:10:38 +0200

    refactor: replace direct GIT calls with qstrip wrapper in external.mk

--- external.mk
+++ external.mk
@@ -1,8 +1,8 @@
-EXTERNAL_ML_OSS_REFERENCE=$(shell pushd $(BR2_EXTERNAL_ML_OSS_PATH) &> /dev/null && $(GIT) rev-parse --short HEAD; popd &> /dev/null)
-EXTERNAL_ML_OSS_DIRTY=$(shell pushd $(BR2_EXTERNAL_ML_OSS_PATH) &> /dev/null && $(GIT) diff --shortstat 2>/dev/null | wc -l ; popd &> /dev/null)
+EXTERNAL_ML_OSS_REFERENCE=$(shell pushd $(BR2_EXTERNAL_ML_OSS_PATH) &> /dev/null && $(call qstrip,$(BR2_GIT)) rev-parse --short HEAD; popd &> /dev/null)
+EXTERNAL_ML_OSS_DIRTY=$(shell pushd $(BR2_EXTERNAL_ML_OSS_PATH) &> /dev/null && $(call qstrip,$(BR2_GIT)) diff --shortstat 2>/dev/null | wc -l ; popd &> /dev/null)
 
-BUILDROOT_REFERENCE=$(shell $(GIT) rev-parse --short HEAD)
-BUILDROOT_DIRTY=$(shell $(GIT) diff --shortstat 2>/dev/null | wc -l)
+BUILDROOT_REFERENCE=$(shell $(call qstrip,$(BR2_GIT)) rev-parse --short HEAD)
+BUILDROOT_DIRTY=$(shell $(call qstrip,$(BR2_GIT)) diff --shortstat 2>/dev/null | wc -l)
 
 include $(sort $(wildcard $(BR2_EXTERNAL_ML_OSS_PATH)/package/*/*.mk))
 include $(BR2_EXTERNAL_ML_OSS_PATH)/adaptation/adaptation.mk
