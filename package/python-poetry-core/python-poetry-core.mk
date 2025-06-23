9c57d19a1a0162374150684691f32b87bbc7e6d0
Bram Oosterhuis <dev@bybram.com>
2025-06-23 14:25:59 +0200

    Development/rpi5 rebase (#34)
    * [rpi5]: create rpi5 board configurations
    
    * with BR 2024.11.1 tag we do not need these packages from ml-opensource
    
    * [wpewebkit-ml]: bump up 2.46
    
    * update libwpe for wpe2.46
    
    * bump up 2.46 version and fix WPEWEBKIT_CONF_OPTS of GBM for ML version
    
    * rpi5: update defconfig
    
    * Revert "with BR 2024.11.1 tag we do not need these packages from ml-opensource"
    
    This reverts commit fe26022e5027e3a4bc4c970e684138b35307a533.
    
    * Moved the patch to a versions specific folder
    
    * Detect if python-poetry-core is in already part of buildroot
    
    * configs: rename Raspberry Pi 5 configuration file
    
    * config: regenerated defconfig
    
    ---------
    
    Co-authored-by: modeveci <o.deveci@metrological.com>

--- package/python-poetry-core/python-poetry-core.mk
+++ package/python-poetry-core/python-poetry-core.mk
@@ -3,7 +3,7 @@
 # python-poetry-core
 #
 ################################################################################
-
+ifeq ($(PYTHON_POETRY_CORE_VERSION),)
 PYTHON_POETRY_CORE_VERSION = 1.7.0
 PYTHON_POETRY_CORE_SOURCE = poetry_core-$(PYTHON_POETRY_CORE_VERSION).tar.gz
 PYTHON_POETRY_CORE_SITE = https://files.pythonhosted.org/packages/cb/1c/af7f886e723b2dfbaea9b8a739153f227b386dd856cf956f9fd0ed0a502b
@@ -12,3 +12,4 @@ PYTHON_POETRY_CORE_LICENSE = MIT
 PYTHON_POETRY_CORE_LICENSE_FILES = LICENSE
 
 $(eval $(host-python-package))
+endif
